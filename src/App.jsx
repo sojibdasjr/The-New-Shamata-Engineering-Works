718910011a3a199f467205a68ceda7f754ea8e4f
sojibdasjr <contact.sojibdas@gmail.com>
2024-03-12 02:10:16 +0600

    creact project install router and install daysi install tailwind and setup seconday Navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SecondaryNavbar from "./components/SecondaryNavbar/SecondaryNavbar";
+
+function App() {
+  return (
+    <>
+      <SecondaryNavbar />
+    </>
+  );
+}
+
+export default App;
