96a8dbcc1ada6a763f9b5c2ebf8d9fb0fa61f73b
sojibdasjr <contact.sojibdas@gmail.com>
2024-03-12 21:22:47 +0600

    fix fotter

--- src/components/GotoUp/GoToUp.jsx
+++ src/components/GotoUp/GoToUp.jsx
@@ -3,14 +3,13 @@ import ScrollToTop from "react-scroll-to-top";
 
 const GoToUp = () => {
   return (
-    <div className="bg-sky-400">
+    <div>
       <ScrollToTop
         className="flex justify-center items-center  "
         smooth
         top="1000"
         color="rgb(2 132 199)"
       />
-      ;
     </div>
   );
 };
